932e0de8783f0e0d5c557638255212ff7a31a02d
Yvette Rosario <vette.rosario@gmail.com>
2023-01-11 23:37:53 -0500

    added logo image to main page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,11 +1,18 @@
+import { NavLink } from 'react-router-dom';
+import "./MainPage.css"
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
+      <h1 className="display-5 fw-bold">Motorized</h1>
       <div className="col-lg-6 mx-auto">
+      <img
+          src={"./logo.png"}
+          style={{ width: 400, height: 400 }}
+        ></img>
+
         <p className="lead mb-4">
-          The premiere solution for automobile dealership
-          management!
+          The optomized management system for your dealership needs
         </p>
       </div>
     </div>
